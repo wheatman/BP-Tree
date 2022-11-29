149f4d60fcb23ce183efe6537af33274228097c5
dabao12321 <amandali@mit.edu>
2022-11-29 02:10:16 +0000

    implemented parallel merge

--- btree_tests/test.cpp
+++ btree_tests/test.cpp
@@ -1592,7 +1592,7 @@ test_iterator_merge_range_version_map(uint64_t max_size, std::seed_seq &seed, bo
   typedef tlx::btree_map<T, T, std::less<T>, tlx::btree_default_traits<T, T, internal_bytes>,
           std::allocator<T>, true> btree_type;
 
-  for(int i = 0; i < num_trials; i++) {
+  for(int trial = 0; trial < num_trials; trial++) {
       tlx::btree_map<T, T, std::less<T>, tlx::btree_default_traits<T, T, internal_bytes>,
               std::allocator<T>, true> merged_tree, merged_unsorted_tree;
 
@@ -1616,43 +1616,6 @@ test_iterator_merge_range_version_map(uint64_t max_size, std::seed_seq &seed, bo
                 });
       end_time = get_usecs();
       printf("\tDone sweeping %lu elts via sorted range in %lu\n", max_size, end_time - start_time);
-
-      /*      
-      auto iterator1 = vec1.begin();
-      auto iterator2 = vec2.begin();
-
-      start_time = get_usecs();
-      while (iterator1 != vec1.end() && iterator2 != vec2.end()) {
-          auto val1 = *iterator1;
-          auto val2 = *iterator2;
-          if (std::get<0>(val1) < std::get<0>(val2)) {
-              merged_tree.insert({std::get<0>(val1), std::get<1>(val1)});
-              iterator1++;
-          } else {
-              merged_tree.insert({std::get<0>(val2), std::get<1>(val2)});
-              iterator2++;
-          }
-      }
-      if (iterator1 != vec1.end()) {
-          auto val1 = *iterator1;
-          while (iterator1 != vec1.end()) {
-              merged_tree.insert({std::get<0>(val1), std::get<1>(val1)});
-              iterator1++;
-          }
-      } else if (iterator2 != vec2.end()) {
-          auto val2 = *iterator2;
-          while (iterator2 != vec2.end()) {
-              merged_tree.insert({std::get<0>(val2), std::get<1>(val2)});
-              iterator2++;
-          }
-      } else {
-          std::cout << "Invalid scenario!\n";
-          exit(0);
-      }
-
-      end_merge_time = get_usecs();
-      printf("\t\tDone serial merging sorted %lu elts via iterator in %lu\n", max_size, end_merge_time - start_time);
-      */
       
       // std::mt19937_64 eng(0);
       // std::shuffle(std::begin(vec1), std::end(vec1), eng);
@@ -1669,6 +1632,143 @@ test_iterator_merge_range_version_map(uint64_t max_size, std::seed_seq &seed, bo
       end_merge_time = get_usecs();
       printf("\t\tDone concurrent merging sorted %lu elts via sorted_range output in %lu\n", max_size, end_merge_time - start_time);
 
+  }
+  return true;
+}
+
+template <class T, uint32_t internal_bytes>
+bool
+test_parallel_merge_map(uint64_t max_size, uint64_t num_chunk_multiplier, std::seed_seq &seed, bool write_csv, int num_trials) {
+  uint64_t start_time, end_time;
+
+  uint64_t num_chunks = 48 * num_chunk_multiplier;
+  uint64_t chunk_size = std::numeric_limits<T>::max() / num_chunks;
+
+  printf("\nRunning leafds btree with internal bytes = %u with leafds slots %lu\n",internal_bytes, SLOTS);
+
+  std::vector<T> data =
+      create_random_data<T>(max_size, std::numeric_limits<T>::max(), seed);
+  
+  std::set<T> checker_set;
+  bool wrong;
+  tlx::btree_map<T, T, std::less<T>, tlx::btree_default_traits<T, T, internal_bytes>,
+                std::allocator<T>, true> concurrent_map1, concurrent_map2;
+
+  // TIME INSERTS
+  start_time = get_usecs();
+  cilk_for(uint32_t i = 0; i < max_size; i++) {
+    concurrent_map1.insert({data[i], 2*data[i]});
+  }
+  end_time = get_usecs();
+  printf("\tDone inserting %lu elts in %lu\n",max_size, end_time - start_time);
+
+#if CORRECTNESS
+  for (uint32_t i = 0; i < max_size; i++) {
+    checker_set.insert(data[i]);
+  }
+
+  std::vector<T> elts_sorted;
+  for (auto key: checker_set) {
+    elts_sorted.push_back(key);
+  }
+  std::sort(elts_sorted.begin(), elts_sorted.end());
+#endif
+
+  std::seed_seq seed2{1};
+  std::vector<T> data2 =
+          create_random_data<T>(max_size, std::numeric_limits<T>::max(), seed2);
+
+  cilk_for(uint32_t i = 0; i < max_size; i++) {
+      concurrent_map2.insert({data2[i], 2*data2[i]});
+  }
+  
+#if CORRECTNESS
+  for (uint32_t i = 0; i < max_size; i++) {
+    checker_set.insert(data2[i]);
+  }
+
+  std::vector<std::tuple<T,T>> elts_sorted_merged;
+  for (auto key: checker_set) {
+    elts_sorted_merged.push_back({key, 2 * key});
+  }
+  std::sort(elts_sorted_merged.begin(), elts_sorted_merged.end());
+#endif
+
+  typedef tlx::btree_map<T, T, std::less<T>, tlx::btree_default_traits<T, T, internal_bytes>,
+          std::allocator<T>, true> btree_type;
+
+  for(int trial = 0; trial < num_trials; trial++) {
+    // tlx::btree_map<T, T, std::less<T>, tlx::btree_default_traits<T, T, internal_bytes>,
+    //         std::allocator<T>, true> merged_tree;
+    
+    std::vector<std::vector<std::tuple<T, T>>> merged_vecs(num_chunks);
+    std::vector<uint64_t> merged_vecs_prefix_sums(num_chunks + 1);
+    merged_vecs_prefix_sums[0] = 0;
+    
+    start_time = get_usecs();
+
+    cilk_for (uint64_t chunk_idx = 0; chunk_idx < num_chunks; chunk_idx++) {
+      std::vector<std::tuple<T, T>> vec1, vec2;
+      uint64_t start_key = 1 + chunk_idx * chunk_size;
+      uint64_t end_key = (chunk_idx == num_chunks - 1) ? std::numeric_limits<T>::max() : 1 + (chunk_idx + 1) * chunk_size;
+
+      uint64_t count_elts1 = 0;
+      uint64_t count_elts2 = 0;
+
+      uint64_t start_time, end_time, end_merge_time;
+      start_time = get_usecs();
+
+      concurrent_map1.map_range_sorted_end(start_key, end_key, [&vec1, &count_elts1]([[maybe_unused]] auto key, auto val) {
+                  vec1.push_back({key, val});
+                  count_elts1++;
+                });
+      concurrent_map2.map_range_sorted_end(start_key, end_key, [&vec2, &count_elts2]([[maybe_unused]] auto key, auto val) {
+                  vec2.push_back({key, val});
+                  count_elts2++;
+                });
+      std::vector<std::tuple<T, T>> merged_chunk;
+      // merged_chunk.reserve(count_elts1 + count_elts2);
+
+      std::merge(vec1.begin(), vec1.end(), vec2.begin(), vec2.end(), std::back_inserter(merged_chunk));
+      merged_vecs[chunk_idx] = merged_chunk;
+      merged_vecs_prefix_sums[chunk_idx + 1] = merged_chunk.size();
+    }
+
+    end_time = get_usecs();
+    printf("\tDone merging chunks for %lu elts via sorted range end in %lu\n", max_size, end_time - start_time);
+
+
+    start_time = get_usecs();
+    for (size_t chunk_idx = 1; chunk_idx < num_chunks + 1; chunk_idx++) {
+      merged_vecs_prefix_sums[chunk_idx] += merged_vecs_prefix_sums[chunk_idx - 1];
+    }
+    end_time = get_usecs();
+    uint64_t summing_time = end_time - start_time;
+    
+    std::vector<std::tuple<T, T>> concat_merged(merged_vecs_prefix_sums[num_chunks]);
+
+    start_time = get_usecs();
+    cilk_for (size_t chunk_idx = 0; chunk_idx < num_chunks; chunk_idx++) {
+      uint64_t count = 0;
+      for (uint64_t index = merged_vecs_prefix_sums[chunk_idx]; index < merged_vecs_prefix_sums[chunk_idx + 1]; index++) {
+        concat_merged[index] = merged_vecs[chunk_idx][count];
+        count++;
+      }
+    }
+    end_time = get_usecs();
+    printf("\tDone concating chunks for %lu elts via sorted range end in %lu\n", max_size, summing_time + end_time - start_time);
+
+#if CORRECTNESS
+    if (elts_sorted_merged != concat_merged) {
+      printf("\tMerged vector not correct\n");
+      for (uint64_t i = 0; i < elts_sorted_merged.size(); i++) {
+        if (elts_sorted_merged[i] != concat_merged[i]) {
+          printf("wrong at index %lu , correct = %lu, wrong = %lu\n", i, std::get<0>(elts_sorted_merged[i]), std::get<0>(concat_merged[i]));
+        }
+      }
+      return false;
+    }
+#endif
   }
   return true;
 }
@@ -1693,9 +1793,10 @@ int main(int argc, char *argv[]) {
   outfile << "tree_type, internal bytes, leaf slots, num_inserted,num_range_queries, max_query_size,  unsorted_query_time, sorted_query_time, \n";
   outfile.close();
 
+  bool correct = test_parallel_merge_map<unsigned long, 1024>(n, num_queries, seed, write_csv, trials);
   // bool correct = test_iterator_merge_range_version_map<unsigned long, 1024>(n, seed, write_csv, trials);
   // bool correct = test_iterator_merge_map<unsigned long, 1024>(n, seed, write_csv, trials);
-  bool correct = test_concurrent_microbenchmarks_map<unsigned long, 1024>(n, num_queries, seed, write_csv, trials);
+  // bool correct = test_concurrent_microbenchmarks_map<unsigned long, 1024>(n, num_queries, seed, write_csv, trials);
 
   if (!correct) {
     printf("got the wrong answer :(\n");
